7cbce50f879fc20f140d3750a35299bcc2c299e2
Or Galili <orgalil21@gmail.com>
2021-07-09 11:52:31 +0300

    adding Inotyphypropertychanged in order to date time to respond for changes

--- EnviTechTest/DataModel.cs
+++ EnviTechTest/DataModel.cs
@@ -1,16 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EnviTechTest
 {
-    class DataModel
+    class DataModel: INotifyPropertyChanged
     {
-        public DateTime Date { get; set; }
+        private DateTime? fromDate;
+        public DateTime? FromDate
+        {
+            get { return fromDate; }
+            set { fromDate = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private DateTime? tillDate;
+        public DateTime? TillDate
+        {
+            get { return tillDate; }
+            set { tillDate = value;
+                OnPropertyChanged();
+            }
+        }
 
-        public List<float> Value { get; set; }
+        //public List<float> Value { get; set; }
+        private List<string> value = new List<string>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public List<string> Value { get {return value; } set { this.value = value; } }
 
         public List<int> Status { get; set; }
     }
